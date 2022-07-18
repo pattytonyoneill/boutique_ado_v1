22d7ddf46aa73938f3fb421efb0428fcc3050465
pattytonyoneill <pattytonyoneill@gmail.com>
2022-07-18 16:16:14 +0000

    Edit of settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -2,7 +2,7 @@ import os
 import dj_database_url
 
 if os.path.exists('env.py'):
-    import env.py
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
