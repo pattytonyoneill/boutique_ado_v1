d265c028e499eae5e6429c4da0edb901953bb537
pattytonyoneill <pattytonyoneill@gmail.com>
2022-07-20 12:05:02 +0000

    Add DEPLOYMENT to env.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -112,7 +112,7 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600)
     }
 else:
     DATABASES = {
