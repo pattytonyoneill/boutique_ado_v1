d113e509dad65093e2e118f2d3bb85d58e0297e8
pattytonyoneill <pattytonyoneill@gmail.com>
2022-07-25 14:51:31 +0000

    Edit of views.py

--- bag/views.py
+++ bag/views.py
@@ -43,7 +43,7 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
-    
+
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
@@ -90,10 +90,12 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
-            messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
+            messages.success(
+                request, f'Removed size {size.upper()} {product.name} from your bag')
         else:
             bag.pop(item_id)
-            messages.success(request, f'Removed {product.name} from your bag')
+            messages.success(
+                request, f'Removed {product.name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
